3289983d07b1ef8fec6ae79e2b2beded1bc14b8e
RitheeshBaradwaj <ritheeshbaradwaj@gmail.com>
2023-09-17 03:16:47 +0900

    Add pull request template

--- tools/setup.sh
+++ tools/setup.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
+# TODO: Update this script for other OS & able to reproducible
+
 brew install bazelisk
 
 # for linting
@@ -9,6 +11,7 @@ brew install pre-commit
 curl -sS https://webi.sh/shfmt | sh
 source "$HOME/.config/envman/PATH.env"
 brew install shellcheck
+brew install actionlint
 
 pre-commit install
 pre-commit run -a
